b737c2d205f04b3055c9e94132ed05429086b95a
Brett Goss <brettgoss@live.com>
2016-11-01 09:33:25 -0700

    Chatty App initial commit with boilerplate set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
 class App extends Component {
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div className="wrapper">
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <div id="message-list">
+          <Message />
+          <MessageList />
+        </div>
+        <ChatBar />
+      </div>
     );
   }
 }
